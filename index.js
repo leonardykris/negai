c9fefec5d4eee68e4da97d3d6e5615f3439c29b3
Leonardy Kristianto <alendory@gmail.com>
2017-06-22 10:06:17 +0700

    added support for lengthy conversion now with splicing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require("http");
 var server = http.createServer(app);
 var request = require("request");
 var io = require('socket.io')(server);
+var encodeUrl = require('encodeurl');
 // var Dictionary = require("japaneasy");
 // var dict = new Dictionary({
 //   dictionary: "auto",
@@ -15,6 +16,7 @@ var io = require('socket.io')(server);
 // });
 
 kuroshiro.init();
+// console.log(encodeUrl("願い"));
 
 // dict("辞書").then(function(result) {
 //   console.log(result);
@@ -35,8 +37,40 @@ io.on('connection', function(socket){
 
   socket.on('kanji-input', function(message){
     // console.log('input: ' + message);
-    var transformed = kuroshiro.convert(message, {mode: "furigana"});
-    io.emit('kanji-output', transformed);
+    var limit = 16;
+    var placeholder = "";
+
+    // Lengthy input
+    if (message.length > limit) {
+      // var test = "向かい風ばかり なんで自分だけ 綺麗ごとが 嫌いだった";
+      // chunks = message.split(" ");
+      paragraphs = message.split("  ");
+
+      paragraphs.forEach(function(element, index) {
+        chunks = element.split(" ");
+
+        chunks.forEach(function(slice, index) {
+          slice = placeholder + "  " + slice;
+          if (slice.length < limit) {
+            placeholder = slice;
+          } else {
+            io.emit('kanji-output', kuroshiro.convert(slice, {mode: "furigana"}));
+            placeholder = "";
+          }
+        });
+
+        if (placeholder !== "") {
+          io.emit('kanji-output', kuroshiro.convert(placeholder, {mode: "furigana"}));
+          placeholder = "";
+        }
+
+        io.emit('kanji-output', "&nbsp;");
+      });
+
+
+    }
+    // var transformed = kuroshiro.convert(message, {mode: "furigana"});
+    // io.emit('kanji-output', transformed);
   });
 
   socket.on('translate-input', function(message){
@@ -52,15 +86,26 @@ io.on('connection', function(socket){
         var japanese = record.japanese[0].word;
         var reading = record.japanese[0].reading;
         var senses = record.senses[0].english_definitions[0];
-        console.log(japanese + " (" + reading + ") " + ": " + senses);
+        var formatted = japanese + " (" + reading + ") " + ": " + senses;
+
+        console.log(formatted);
       });
+    }
+
+    var getFirstResult = function(result) {
+      var japanese = result[0].japanese[0].word;
+      var reading = result[0].japanese[0].reading;
+      var senses = result[0].senses[0].english_definitions[0];
+      var formatted = japanese + " (" + reading + ") " + ": " + senses;
+
+      io.emit('translate-output', formatted);
     }
 
     var word = message;
     console.log('input      : ' + word + " (" + kuroshiro.convert(word) + ")");
 
     var options = {
-      uri: "http://jisho.org/api/v1/search/words?keyword=" + word,
+      uri: "http://jisho.org/api/v1/search/words?keyword=" + encodeUrl(word),
       method: "GET",
       timeout: 10000,
       followRedirect: true,
@@ -74,12 +119,7 @@ io.on('connection', function(socket){
       // console.log('body: ', body);
 
       var result = body.data;
-      list(result);
-
-
-      // console.log(result);
-
-
+      getFirstResult(result);
     });
   });
 
