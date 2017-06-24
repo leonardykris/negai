ffd956bc2f738212b6b2f8d72beb9acb475b1d36
Leonardy Kristianto <alendory@gmail.com>
2017-06-24 22:59:05 +0700

    added temp song library

---
+++ songs.js
@@ -0,0 +1,10 @@
+var songs = {
+  "bnha-ed-1": {
+    "name": "だから、一人じゃない",
+    "name-alt": "Dakara, hitori janai",
+    "series": "Boku no Hero Academia",
+    "lyric": "向かい風ばかり なんで自分だけ\n綺麗ごとが 嫌いだった\nすべては繋がる 今この瞬間\n走り続けなきゃ 夢に近づけない\n\n空見上げてるだけじゃ 答えは見つからないんだ\n今こそ 証明したいよ 僕らが ココにいることを"
+  }
+}
+
+module.exports = songs;
